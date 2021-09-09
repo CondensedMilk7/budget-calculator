4933fd30407c301438891c9732c0738eba31a489
CondensedMilk7 <82287873+CondensedMilk7@users.noreply.github.com>
2021-09-09 14:39:27 +0400

    inital commit]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { AddItemFormComponent } from './add-item-form/add-item-form.component';
+import { BudgetItemListComponent } from './budget-item-list/budget-item-list.component';
+import { BudgetItemCardComponent } from './budget-item-list/budget-item-card/budget-item-card.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent,
+    AddItemFormComponent,
+    BudgetItemListComponent,
+    BudgetItemCardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/budget-item-list/budget-item-card/budget-item-card.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-budget-item-card',
+  templateUrl: './budget-item-card.component.html',
+  styleUrls: ['./budget-item-card.component.scss'],
+})
+export class BudgetItemCardComponent implements OnInit {
+  @Input() isIncome: boolean = true;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
